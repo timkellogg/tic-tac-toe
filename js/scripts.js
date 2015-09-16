229e06cad5a162ad98f4f5f0c056c10d8ab4c671
Janice Laset-Parkerson & Aileen Cacayorin <student@epicodus.com>
2015-09-16 11:24:49 -0700

    win prototype checks to see if a player has won

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,62 @@
-function Player (mark, spaces) {
+// Used MDN array protoype so that we can have an includes method
+
+
+Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+  'use strict';
+  var O = Object(this);
+  var len = parseInt(O.length) || 0;
+  if (len === 0) {
+    return false;
+  }
+  var n = parseInt(arguments[1]) || 0;
+  var k;
+  if (n >= 0) {
+    k = n;
+  } else {
+    k = len + n;
+    if (k < 0) {k = 0;}
+  }
+  var currentElement;
+  while (k < len) {
+    currentElement = O[k];
+    if (searchElement === currentElement ||
+      (searchElement !== searchElement && currentElement !== currentElement)) {
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+
+
+
+
+
+function Player (mark) {
   this.mark = mark;
   this.spaces = [];
+  // this.win = false
 }
 
 Player.prototype.move = function(move) {
   this.spaces.push(move);
-}
+};
+
+// this prototype tests to see if a player has a winning combination of spaces
+
+Player.prototype.win = function(spaces) {
+  var result = [];
+  var winningCombos = [[1, 2, 3], [4, 5, 6,], [7, 8, 9], [1, 4, 7], [2, 5 ,8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+  var win =  false;
+  winningCombos.forEach(function(combo) {
+    debugger;
+    for (var i = 0; i <3; i++) {
+      result[i] = spaces.includes(combo[i]);
+    }
+    if ((result[0] === true) && (result[1] === true) && (result[2] === true)) {
+       win = true;
+    }
+
+ });
+ return win;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,22 @@
 describe("Player", function() {
   it("returns the player's mark", function() {
-    var testPlayer1 = new Player("X", []);
+    var testPlayer1 = new Player("X");
     expect(testPlayer1.mark).to.equal("X");
   });
 
   it('allows a player to pick a space', function(){
-    var testPlayer1 = new Player("X", []);
+    var testPlayer1 = new Player("X");
     testPlayer1.move(1)
     expect(testPlayer1.spaces).to.eql([1]);
   });
 
+  it('checks if a player has a winning combination', function() {
+    var testPlayer1 = new Player("X");
+    testPlayer1.move(4);
+    testPlayer1.move(5);
+    testPlayer1.move(6);
+    expect(testPlayer1.win(testPlayer1.spaces)).to.equal(true);
+  });
 });
 
 // describe('Space', function(){
